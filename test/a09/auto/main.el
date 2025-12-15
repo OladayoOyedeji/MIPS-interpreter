0d67efa9b82e0a92ef2f31211bb1713ecfcf2963
Oyedeji <robertoyedeji7@gmail.com>
2025-12-15 06:47:54 -0800

    sync

--- test/a09/auto/main.el
+++
@@ -1,7 +0,0 @@
-(TeX-add-style-hook "main"
- (lambda ()
-    (TeX-run-style-hooks
-     "myassignmentpreamble"
-     "ciss360"
-     "yliow")))
-
--- test/a10/tmp/main.cpp
+++
@@ -1,9 +0,0 @@
-#include <iostream>
-
-int main()
-{
-    int x = 234523;
-    std::cout << (x + (~x)) << '\n';
-
-    return 0;
-}
