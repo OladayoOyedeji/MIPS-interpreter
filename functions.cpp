5c52603ace84c96a14494a086e4e937560fdaa9a
OladayoOyedeji <robertoyedeji7@gmail.com>
2025-12-16 15:48:14 -0600

    Clean up functions.cpp by removing comments
    Removed commented-out debug output and unused functions.

--- functions.cpp
+++ functions.cpp
@@ -38,32 +38,26 @@ off_t File::mylseek(off_t offset, int whence)
 void append_to_path(char * path, int & size, const char * file)
 {
     // append dir to path
-    // std::cout << "herro? [" << path[size - 2] << ']' << std::endl;
     path[size++] = '/';
     int i = 0;
     while (true)
     {
-        // std::cout << '[' << path << "] " << file[i] << std::endl;
         path[size] = file[i];
         if (file[i++] == '\0') break;
         size++;
     }
-    // std::cout << '[' << path << "] " << std::endl;
 }
 
 void append_to_path(std::string & path, const char * file)
 {
     // append dir to path
-    // std::cout << "herro? [" << path[size - 2] << ']' << std::endl;
     path.push_back('/');
     int i = 0;
     while (true)
     {
-        // std::cout << '[' << path << "] " << file[i] << std::endl;
         path.push_back(file[i]);
         if (file[i++] == '\0') break;
     }
-    // std::cout << '[' << path << "] " << std::endl;
 }
 
 int32_t len(const char * s)
@@ -77,15 +71,12 @@ int32_t len(const char * s)
 
 bool strcmp(const std::string & s, const std::string & c)
 {
-    // std::cout << '[' << s << ' ' << c << ']' << std::endl;
-    // std::cout << s.size() << ' ' << c.size() << std::endl;
     if (s.size() != c.size())
     {
         return false;
     }
     for (int i = 0; i < s.size(); ++i)
     {
-        // std::cout << s[i] << ' ' << c[i] << std::endl;
         if (s[i] != c[i])
             return false;
     }
@@ -192,7 +183,6 @@ int get_numeric(const std::string & s)
         if (s[i] <= '9' && s[i] >= '0')
         {
             num += (s[i] - '0') * pow(10, size - i - 1);
-            // std::cout << num << ' ' << s[i] << std::endl;
         }
         else
         {
@@ -208,20 +198,14 @@ int get_register(const std::string & s)
 {
     if (REGISTER_NOMENCLATURE.find(s) != REGISTER_NOMENCLATURE.end())
     {
-        // std::cout << "here?\n" << "rt" << s << ' '
-        //           << REGISTER_NOMENCLATURE[s] << std::endl;
         return REGISTER_NOMENCLATURE[s];
     }
     else if (s.size() == 2 && s[0] == '$' && s[1] >= '0' && s[1] <= '9')
     {
-        // std::cout << "no here?\n" << "rt" << s << ' ' << s[1] << std::endl;;
         return (s[1] - '0');
     }
     else
     {
-        // std::string error_message = "Register ";
-        // error_message.push_back(s);
-        // error_message.push_back("not found");
         throw std::runtime_error("Register not found");
     }
     return -1;
@@ -348,7 +332,6 @@ void data_lexer(const std::string & s, std::vector<std::string> & token, std::st
             if (s[i] == '\"' || s[i] == '\'')
             {
                 char c = s[i];
-                //token.push_back(c);
                 j = ++i;
                 while (s[j] != c)
                 {
@@ -406,14 +389,3 @@ void print_unsigned_char(unsigned char c)
     }
     
 }
-
-
-
-// unsigned char * uint_to_hexad(int32_t i)
-// {}
-// unsigned char * uint_to_buff(int32_t i)
-// {}
-// int32_t hexad_to_uint(unsigned char buff[])
-// {}
-// int32_t buff_to_int(unsigned char buff[])
-// {}
